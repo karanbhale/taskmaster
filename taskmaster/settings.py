e97766961f5cf69035036166e61fd0b472a809bb
karanbhale <bhalekaran8@gmail.com>
2022-07-25 23:03:42 +0530

    change requirement for gunicorn

--- taskmaster/settings.py
+++ taskmaster/settings.py
@@ -115,6 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 SATTICFILES_DIRS = [(os.path.join(BASE_DIR,'static'))]
 
@@ -128,5 +129,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "todolist"
 
 LOGIN_URL ="login"
+
 
 django_heroku.settings(locals())
